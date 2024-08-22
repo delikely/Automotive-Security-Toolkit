ab5b6ec6b5e70c3895b0148c99257212b920299d
delikely <delikely@gmail.com>
2024-08-22 14:34:40 +0800

    bug fix

--- cross-border checker.py
+++ cross-border checker.py
@@ -1,5 +1,5 @@
-# Date： 2023/05/12
-# Version: 1.0
+# Date： 2023/08/22
+# Version: 1.1
 # Author: delikely
 # Description：脚本用验证《汽车整车信息安全技术要求》（征求意见稿）中提到跨境传输检测要求，“使用网络数据抓包工具进行不少于3600秒的数据抓包，解析通信报文数据，分析目的IP地址中是否包含境外IP地址，并记录测试结果，应不包含境外IP地址。”
 
@@ -28,7 +28,8 @@ def is_public_ip(ip):
 def cross_border_addr(ip):
     # 百度IP信息查询API
     query_api = "https://opendata.baidu.com/api.php?co=&resource_id=6006&oe=utf8&query="
-    homeland = ["中国","北京","天津","河北","山西","内蒙古","辽宁","吉林","黑龙江","上海","江苏","浙江","安徽","福建","江西","山东","河南","湖北","湖南","广东","广西","海南","重庆","四川","贵州","云南","西藏","陕西","甘肃","青海","宁夏","新疆"] 
+    # homeland = ["中国","北京","天津","河北","山西","内蒙古","辽宁","吉林","黑龙江","上海","江苏","浙江","安徽","福建","江西","山东","河南","湖北","湖南","广东","广西","海南","重庆","四川","贵州","云南","西藏","陕西","甘肃","青海","宁夏","新疆"]
+    homeland = ["中国","北京","天津","河北","山西","内蒙","辽宁","吉林","黑龙","上海","江苏","浙江","安徽","福建","江西","山东","河南","湖北","湖南","广东","广西","海南","重庆","四川","贵州","云南","西藏","陕西","甘肃","青海","宁夏","新疆"]  
     response = requests.get(query_api+ip)
     location = json.loads(response.content.decode())["data"][0]["location"]
     if location[0:2] not in homeland:
@@ -50,6 +51,8 @@ def cross_border_check(pcap):
                 ip_list.append(src_ip)
             if dst_ip not in ip_list:
                 ip_list.append(dst_ip)
+
+    ip_list = list(set(ip_list))
     
     for ip in ip_list:
         if is_public_ip(ip):
